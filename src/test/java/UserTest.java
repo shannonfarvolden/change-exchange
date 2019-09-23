67c8d0f7911141313df812bea813c18478796c37
shannonfarvolden <shannonfarvolden@hotmail.com>
2019-09-23 20:47:40 +0100

    Add test for money transfer

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,33 @@
+import User.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class UserTest {
+
+    User user1;
+    User user2;
+
+    @Before
+    public void setup(){
+        user1 = new User(0,"Billy Bob", 10);
+        user2 = new User(0,"Jane Doe", 10);
+
+    }
+
+    @Test
+    public void tranferMoneyAdjustsBalance() throws Exception{
+        user1.transfer(user2, 5);
+
+        assertEquals(15, user1.getBalance());
+        assertEquals(5, user2.getBalance());
+
+    }
+
+    @After
+    public void tearDown(){
+        user1 = null;
+        user2 = null;
+    }
+}
